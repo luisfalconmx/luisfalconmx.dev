9e86e8d49a812d82368cdd0cb084c04942602263
luisfalconmx <luisfalconmx@gmail.com>
2023-01-21 10:54:50 -0600

    feat: button component created

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import Icon from '../assets/icons/icon-arrow.svg'
+import type { ReactNode } from 'react'
+
+type Props = {
+  children: ReactNode
+}
+
+const Button = ({ children }: Props) => {
+  return (
+    <button className="py-4 px-6 bg-dark-400 font-sans font-bold leading-6 text-white rounded-lg first-letter:uppercase cursor-pointer">
+      {children}
+    </button>
+  )
+}
+
+export default Button
