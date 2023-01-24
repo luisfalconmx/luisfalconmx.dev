936b499c544fe9bc76d636eb45f391b85b9caade
luisfalconmx <luisfalconmx@gmail.com>
2023-01-24 00:27:33 -0600

    feat: footer component created with social menu and main menu

---
+++ src/data/socialMenu.ts
@@ -0,0 +1,36 @@
+import type { SocialMenu } from '../types/SocialMenu'
+import facebookIcon from '../assets/icons/icon-facebook.svg'
+import instagramIcon from '../assets/icons/icon-instagram.svg'
+import twitterIcon from '../assets/icons/icon-twitter.svg'
+import linkedinIcon from '../assets/icons/icon-linkedin.svg'
+import githubIcon from '../assets/icons/icon-github.svg'
+
+const socialMenu: SocialMenu = [
+  {
+    title: 'facebook',
+    icon: facebookIcon,
+    href: 'https://www.facebook.com/luisfalconmx'
+  },
+  {
+    title: 'instagram',
+    icon: instagramIcon,
+    href: 'https://www.instagram.com/luisfalconmx'
+  },
+  {
+    title: 'twitter',
+    icon: twitterIcon,
+    href: 'https://twitter.com/luisfalconmx'
+  },
+  {
+    title: 'linkedin',
+    icon: linkedinIcon,
+    href: 'https://www.linkedin.com/in/luisfalconmx'
+  },
+  {
+    title: 'github',
+    icon: githubIcon,
+    href: 'https://github.com/luisfalconmx'
+  }
+]
+
+export default socialMenu
---
+++ src/types/SocialMenu.ts
@@ -0,0 +1,5 @@
+export type SocialMenu = {
+  title: string
+  icon: string
+  href: string
+}[]
